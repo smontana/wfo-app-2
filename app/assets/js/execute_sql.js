fbe05988fc6b6909886247f5030cf53b0cf99948
Stephen Montana <stephen.a.montana@gmail.com>
2016-07-11 14:10:05 -0500

    moved the smade the sql execute function its own module

---
+++ app/assets/js/execute_sql.js
@@ -0,0 +1,23 @@
+function execute_sql_and_build_graph (query_selection, graph_builder_obj) {
+  if (Array.isArray(graph_builder_obj)) {
+    var sql_procedure = sql.execute({
+      query: query_selection
+    }).then(function (data) {
+      _.each(graph_builder_obj, function (x) { return x(data); })
+    }, function (err) {
+      console.log(err)
+    })
+  } else {
+    var sql_procedure = sql.execute({
+      query: query_selection
+    }).then(function (data) {
+      graph_builder_obj(data)
+    }, function (err) {
+      console.log(err)
+    })
+  }
+
+  return sql_procedure
+}
+
+module.exports = execute_sql_and_build_graph
